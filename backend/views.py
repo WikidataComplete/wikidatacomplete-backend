b8a2660db2c50b15769c2495510fdb2ad3450736
lalit97 <lalit.suthar@redhuntlabs.com>
2022-07-08 00:32:40 +0530

    feat: apis for Accept, Reject and Random fact

--- backend/views.py
+++ backend/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from backend.models import Fact
 from backend.serializers import FactListCreateSerializer
+from backend.constants import USER_COOKIE, ACCEPT_CORRECTION, REJECT_CORRECTION
 
 
 class FactListCreateAPI(generics.ListCreateAPIView):
@@ -26,3 +27,48 @@ class RetrieveFactWithQId(APIView):
         except Exception as e:
             return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
         return Response(response, status=status.HTTP_200_OK)
+
+
+class RetrieveRandomFact(APIView):
+    def get(self, request, *args, **kwargs):
+        try:
+            response = requests.get(
+                "https://qanswer-svc3.univ-st-etienne.fr/fact/get?id=EMPTY&category=EMPTY&property=EMPTY"
+            ).json()
+        except Exception:
+            return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
+        return Response(response, status=status.HTTP_200_OK)
+
+
+class FactAcceptAPI(APIView):
+    def post(self, request, *args, **kwargs):
+        fact_id = request.data.get("fact_id")
+        if not fact_id and not isinstance(fact_id, str):
+            return Response(
+                {"detail": "Invalid Fact ID."}, status=status.HTTP_400_BAD_REQUEST
+            )
+        try:
+            accept_uri = f"https://qanswer-svc3.univ-st-etienne.fr/fact/correct?userCookie={USER_COOKIE}&factId={fact_id}&correction={ACCEPT_CORRECTION}"
+            requests.post(accept_uri)  # returns text data
+        except Exception:
+            return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
+        return Response(
+            {"message": "Fact Accepted successfully"}, status=status.HTTP_200_OK
+        )
+
+
+class FactRejectAPI(APIView):
+    def post(self, request, *args, **kwargs):
+        fact_id = request.data.get("fact_id")
+        if not fact_id and not isinstance(fact_id, str):
+            return Response(
+                {"detail": "Invalid Fact ID."}, status=status.HTTP_400_BAD_REQUEST
+            )
+        try:
+            reject_uri = f"https://qanswer-svc3.univ-st-etienne.fr/fact/correct?userCookie={USER_COOKIE}&factId={fact_id}&correction={REJECT_CORRECTION}"
+            requests.post(reject_uri)  # returns text data
+        except Exception:
+            return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
+        return Response(
+            {"message": "Fact Rejected successfully"}, status=status.HTTP_200_OK
+        )
