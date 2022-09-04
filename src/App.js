23ddd75281d36cee78bd2b6b9ecf2edcc4356544
lalit97 <sutharlalit.97@gmail.com>
2022-09-05 01:04:31 +0530

    initial react setup and design

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import FileUpload from "./FileUpload";
+
+function App() {
+  return (
+    <div className="App">
+      <FileUpload />
+    </div>
+  );
+}
+
+export default App;
